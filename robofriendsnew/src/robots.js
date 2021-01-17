9447c9d775a1421b232384b10d163da7fe2ac01b
Erik Heinmert <eheinmert@Eriks-MacBook-Pro.local>
2021-01-17 19:30:11 +0100

    initial commit

---
+++ robofriendsnew/src/robots.js
@@ -0,0 +1,48 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email:'sinscere@april.bz'
+    },
+    {
+        id: 2,
+        name: 'brent Graham',
+        username: 'brentif',
+        email:'sinscere@april.bz'
+    },
+    {
+        id: 3,
+        name: 'barry blossom',
+        username: 'brentif',
+        email:'sinscere@april.bz'
+    }
+    ,
+    {
+        id: 4,
+        name: 'barry blossom',
+        username: 'brentif',
+        email:'sinscere@april.bz'
+    }
+    ,
+    {
+        id: 5,
+        name: 'barry blossom',
+        username: 'brentif',
+        email:'sinscere@april.bz'
+    }
+    ,
+    {
+        id: 6,
+        name: 'barry blossom',
+        username: 'brentif',
+        email:'sinscere@april.bz'
+    }
+    ,
+    {
+        id: 7,
+        name: 'barry blossom',
+        username: 'brentif',
+        email:'sinscere@april.bz'
+    }
+]
